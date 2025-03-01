74bf35b7b1c5947d5e5cf62879f565181c247918
Bryan Austin Suharta <66191560+BryanAustinS@users.noreply.github.com>
2025-03-01 19:30:17 +0100

    feature: end game menu

--- aimtrainer.py
+++ aimtrainer.py
@@ -36,14 +36,20 @@ def format_time(secs):
 
     return f"{minutes:02d}:{seconds:02d}.{milli:02d}"
 
-def statistics(win, elapsed_time, target_pressed, misses):
+def statistics(win, elapsed_time, target_pressed, misses, clicks):
     pygame.draw.rect(win, "grey", (0, 0, WIDTH, TOP_BAR_HEIGHT))
 
     time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+
     speed = round(target_pressed / elapsed_time, 1) if elapsed_time > 0 else 0
     speed_label = LABEL_FONT.render(f"Speed: {speed} target/second", 1, "black")
+
     hits_label = LABEL_FONT.render(f"Hits: {target_pressed}", 1, "black")
+
     lives_label = LABEL_FONT.render(f"Lives: {LIVES - misses}", 1, "black")
+
+    accuracy = round(target_pressed / clicks * 100, 1) if clicks > 0 else 0
+    accuracy_label = LABEL_FONT.render(f"Accuracy: {accuracy}%", 1, "black")
 
     # Dynamically position labels based on window width
     margin = 20  # Padding from the left
@@ -53,6 +59,36 @@ def statistics(win, elapsed_time, target_pressed, misses):
     win.blit(speed_label, (spacing * 1, 5))
     win.blit(hits_label, (spacing * 2, 5))
     win.blit(lives_label, (spacing * 3, 5))
+    win.blit(accuracy_label, (spacing * 4, 5))
+
+def end_screen(win, elapsed_time, target_pressed, clicks):
+    win.fill("grey")
+
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+
+    speed = round(target_pressed / elapsed_time, 1)
+    speed_label = LABEL_FONT.render(f"Speed: {speed} target/second", 1, "black")
+
+    hits_label = LABEL_FONT.render(f"Hits: {target_pressed}", 1, "black")
+
+    accuracy = round(target_pressed / clicks * 100, 1)
+    accuracy_label = LABEL_FONT.render(f"Accuracy: {accuracy}%", 1, "black")
+
+    win.blit(time_label, (get_middle(time_label), 100))
+    win.blit(speed_label, (get_middle(speed_label), 200))
+    win.blit(hits_label, (get_middle(hits_label), 300))
+    win.blit(accuracy_label, (get_middle(accuracy_label), 400))
+
+    pygame.display.update()
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                quit()
+
+def get_middle(surface):
+    return WIDTH / 2 - surface.get_width()/2
 
 def main():
     run = True
@@ -106,10 +142,10 @@ def main():
                 targets.remove(target)
 
         if misses >= LIVES:
-            pass # End game
+            end_screen(WIN, elapsed_time, target_pressed, clicks)
 
         draw(WIN, targets)
-        statistics(WIN, elapsed_time, target_pressed, misses)
+        statistics(WIN, elapsed_time, target_pressed, misses, clicks)
         pygame.display.update()
 
     pygame.quit()
